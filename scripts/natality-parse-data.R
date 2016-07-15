b2a58ea62ad56bd21045ba165944433d69587210
Andrew B. Collier <andrew@exegetic.biz>
2016-07-15 18:58:23 +0200

    Properly fixed gender levels!

--- scripts/natality-parse-data.R
+++ scripts/natality-parse-data.R
@@ -11,7 +11,14 @@ births = lapply(list.files("data-raw", pattern = "^nat-.*.bz2", full.names = TRU
   mutate(D,
          month = factor(month, levels = 1:12, labels = month.abb),
          dow = factor(dow, levels = 1:7, labels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")),
-         sex = factor(sex, labels = c("F", "M"))
+         #
+         # Need to start with M as first level because in some years the data are encoded as 1 = M and 2 = F.
+         #
+         sex = factor(sex, labels = c("M", "F")),
+         #
+         # Now relevel so that F is first.
+         #
+         sex = relevel(sex, "F")
   )
 })
 
