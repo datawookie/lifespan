181534ec5764a2080d4700687b263bc543c247bf
Andrew B. Collier <andrew@exegetic.biz>
2016-07-27 13:07:13 +0200

    Bumped version to 0.0.4.

--- R/survival.R
+++ R/survival.R
@@ -7,8 +7,11 @@
 #' If k is not specified then the function will return the probability that a
 #' person aged x will live for at least another t years (they will live up to
 #' at least age x+t).
+#' 
+#' If a value for k is given then the function will return the probability
+#' that a person aged x will live for at least another t years but then die
+#' within the following k years.
 survival <- function(x, t, k = NA) {
-  # the probability that someone aged exactly {\displaystyle \,x} \,x will survive for {\displaystyle \,t} \,t more years, then die within the following {\displaystyle \,k} \,k years
   if (is.na(k)) {
     probabilities = lifetable[lifetable$x %in% c(x, x + t),] %>%
       group_by(sex) %>%
