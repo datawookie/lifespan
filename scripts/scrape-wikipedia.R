fe475325846a4d1238ae21f2e20a1d9381692bc8
Andrew B. Collier <andrew@exegetic.biz>
2016-08-03 06:01:04 +0200

    Improved matching of country names in Wikipedia scrape.

--- scripts/scrape-wikipedia.R
+++ scripts/scrape-wikipedia.R
@@ -8,7 +8,11 @@ population <- read_html("http://bit.ly/29Qqzm3") %>% html_nodes("table") %>% .[[
 population <- population[, c(2:3, 6)]
 names(population) <- c("country", "region", "population")
 population <- mutate(population,
-                     country = str_replace_all(country, "(^[^[:alpha:]]*|\\[.\\]$)", "")
+                     country = str_replace_all(country, "(^[^[:alpha:]]*|\\[.\\]$)", ""),
+                     country = iconv(country, to='ASCII//TRANSLIT'),
+                     country = sub("United States", "United States of America", country),
+                     country = sub("Federated States of Micronesia", "Micronesia", country),
+                     country = sub("Republic of Macedonia", "Macedonia", country)
 )
 
 expectancy <- read_html("http://bit.ly/1LjieDy") %>% html_nodes("table") %>% .[[1]] %>% html_table(trim = TRUE)
@@ -16,7 +20,16 @@ expectancy <- read_html("http://bit.ly/1LjieDy") %>% html_nodes("table") %>% .[[
 expectancy <- expectancy[, c(1, 5, 7)]
 names(expectancy) <- c("country", "female", "male")
 expectancy <- mutate(expectancy,
-                     country = str_replace(country, "^[^[:alpha:]]*", "")
+                     country = str_replace(country, "^[^[:alpha:]]*", ""),
+                     country = iconv(country, to='ASCII//TRANSLIT'),
+                     country = sub("Democratic People's Republic of Korea", "North Korea", country),
+                     country = sub("Republic of Korea", "South Korea", country),
+                     country = sub("Brunei Darussalam", "Brunei", country),
+                     country = sub("Lao People's Democratic Republic", "Laos", country),
+                     country = sub("Republic of Moldova", "Moldova", country),
+                     country = sub("Russian Federation", "Russia", country),
+                     country = sub("United Republic of Tanzania", "Tanzania", country),
+                     country = sub("Viet Nam", "Vietnam", country)
 )
 
 expectancy = merge(population, expectancy) %>% mutate(
@@ -43,3 +56,5 @@ expectancy.plot <- plot_ly(expectancy, x = male, y = female,
     xaxis = list(title = "Male Life Expectancy [years]"),
     yaxis = list(title = "Female Life Expectancy [years]")
   )
+
+# plotly_POST(expectancy.plot, filename = "Exegetic Blog/life-expectancy-by-country")
