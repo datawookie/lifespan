0ec2b04e6eaa59236f303c28ecbc120b52c3dd92
Andrew Collier <andrew@exegetic.biz>
2020-07-10 06:19:13 +0200

    Add plots

--- scripts/various-plots.R
+++ scripts/various-plots.R
@@ -86,3 +86,47 @@ plot_ly(subset(lifetable, sex == "M"), x = x, y = lx / 1000, name = "Male", line
     xaxis = list(title = "Age", dtick = 10),
     yaxis = list(title = "Proportion Living", ticksuffix = "%")
   )
+
+pyramid.files <- sapply(unique(deathsage$year), function(y) {
+  year.data <- subset(deathsage, year == y) %>% mutate(
+    count = ifelse(sex == "F", -count, count)
+  )
+  #
+  g <- ggplot(year.data, aes(x = age, y = count, fill = sex)) +
+    geom_density(data = subset(year.data, sex == "F"), stat = "identity", alpha = 0.75, col = NA) +
+    geom_density(data = subset(year.data, sex == "M"), stat = "identity", alpha = 0.75, col = NA) +
+    scale_y_continuous(
+      limits = c(-47000, 47000),
+      breaks = seq(-40000, 40000, 20000),
+      labels = c(seq(40, 0, -20), seq(20, 40, 20))
+    ) +
+    scale_x_continuous(limits = c(0, 110), breaks = seq(0, 120, 20)) +
+    coord_flip() +
+    scale_fill_manual(values = c("red", "blue", "green")) +
+    labs(y = "Deaths [thousands]", x = "Age") +
+    ggtitle(y) +
+    theme_bw() + theme(legend.title = element_blank())
+  #
+  filename = sprintf("pyramid-%4d.png", y)
+  #
+  png(filename, width = 600, height = 400)
+  print(g)
+  dev.off()
+  #
+  filename
+})
+#
+system(paste("convert -delay 20 -loop 0", paste(pyramid.files, collapse = " "), "pyramid-animation.gif"))
+#
+file.remove(pyramid.files)
+
+# BASEBALL ------------------------------------------------------------------------------------------------------------
+
+plot_baseball_birthdates <- plot_ly(baseball, x = birthdate, type = "histogram") %>%
+  layout(
+    title = "Major League Baseball Birth Dates",
+    xaxis = list(title = "Birth Date"),
+    yaxis = list(title = "Number of Players")
+  )
+
+# plotly_POST(plot_baseball_birthdates, filename = "Exegetic Blog/baseball-birth-dates")
